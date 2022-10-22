253c88438f9379bd4d810b46bd3d35e4d3284778
mingyeom1 <mingyeom@bubagump.net>
2022-10-22 17:53:06 +0900

    Create dummyData

---
+++ public/DummyData.js
@@ -0,0 +1,84 @@
+export const communityListDummy = [
+  {
+    id: 1,
+    title: "피트니스 센터",
+    thumbnail:
+      "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 2,
+    title: "도서관",
+    thumbnail:
+      "https://images.unsplash.com/photo-1521587760476-6c12a4b040da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGlicmFyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 3,
+    title: "요가센터",
+    thumbnail:
+      "https://images.unsplash.com/photo-1588286840104-8957b019727f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8eW9nYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 4,
+    title: "독서실",
+    thumbnail:
+      "https://images.unsplash.com/photo-1495465798138-718f86d1a4bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHN0dWR5JTIwcm9vbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 5,
+    title: "골프연습장",
+    thumbnail:
+      "https://plus.unsplash.com/premium_photo-1661672413008-3bf4d43fdc30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Z29sZnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 6,
+    title: "휴게실",
+    thumbnail:
+      "https://images.unsplash.com/photo-1632181933699-d45280e5c7de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVzdCUyMGFyZWF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+  },
+];
+
+export const communityDetailDummy = [
+  {
+    id: 1,
+    title: "피트니스 센터",
+    description: "입주민을 위한 피트니스 센터입니다",
+    notification: "10월 휴무일 안내입니다.",
+    location: "서울시 송파구 잠실본동 000 하이테크시티 1동 101호-130호",
+    images: [
+      "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    ],
+  },
+  {
+    id: 2,
+    title: "도서관",
+    thumbnail:
+      "https://images.unsplash.com/photo-1521587760476-6c12a4b040da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGlicmFyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 3,
+    title: "요가센터",
+    thumbnail:
+      "https://images.unsplash.com/photo-1588286840104-8957b019727f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8eW9nYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 4,
+    title: "독서실",
+    thumbnail:
+      "https://images.unsplash.com/photo-1495465798138-718f86d1a4bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHN0dWR5JTIwcm9vbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 5,
+    title: "골프연습장",
+    thumbnail:
+      "https://plus.unsplash.com/premium_photo-1661672413008-3bf4d43fdc30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Z29sZnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 6,
+    title: "휴게실",
+    thumbnail:
+      "https://images.unsplash.com/photo-1632181933699-d45280e5c7de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVzdCUyMGFyZWF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+  },
+];
