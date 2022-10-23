4c5ee80ff91030af381c585949e50d5fc0821a5c
daunload <daun4799@gmail.com>
2022-10-23 18:26:00 +0900

    레이아웃 적용

--- src/router/layouts/DefaultLayout.vue
+++ src/router/layouts/DefaultLayout.vue
@@ -1,3 +1,12 @@
 <template>
-  <router-view></router-view>
+    <div class="wrapper">
+        <router-view></router-view>
+    </div>
 </template>
+
+<style scoped lang="scss">
+.wrapper {
+    padding-left: 4vw;
+    padding-right: 4vw;
+}
+</style>
