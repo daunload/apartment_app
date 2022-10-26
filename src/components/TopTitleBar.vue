0274fab5dbd97387c3e6bd731a91797fe0f0f59f
mingyeom1 <mingyeom@bubagump.net>
2022-10-26 21:21:24 +0900

    Create TopTitleBar

---
+++ src/components/TopTitleBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <img src="@/assets/ic-back-white.svg" @click="goBack" />
+    <div class="title">{{ title }}</div>
+    <div />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "TopTitleBar",
+  props: {
+    title: {
+      type: String,
+      require: true,
+    },
+  },
+  methods: {
+    goBack() {
+      this.$router.back();
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: #4c93ff;
+  height: 14.667vw;
+  width: 100vw;
+  padding: 5.333vw 4vw;
+}
+.title {
+  font-size: 4.533vw;
+  color: #fff;
+}
+</style>
--- src/router/views/DetailRegister.vue
+++ src/router/views/DetailRegister.vue
@@ -1,57 +1,60 @@
 <template>
   <div>
-    <MonthSelecter :months="['10월', '11월']" />
-    <usage-info>
-      <div class="title" slot="title">헬스장 월 이용료</div>
-      <div slot="info">
-        <usage-info-text :leftText="'금액'" :rightText="'월 10,000원'" />
-        <usage-info-text
-          :leftText="'유효기간'"
-          :rightText="'10월9일 ~ 10월31일'"
-        />
-        <usage-info-text :leftText="'결제방식'" :rightText="'관리비 청구'" />
-        <usage-info-text
-          :leftText="'정기결제'"
-          :rightText="'매월 1일 결제'"
-          :warningMultiLineText="[
-            '월 단위 이용 가능한 프로그램입니다',
-            '유효기간 종료일을 확인해주세요',
-          ]"
-        />
-        <usage-info-text
-          :leftText="'결제안내'"
-          :multiLineText="[
-            'A-PAY 월 포인트 차감',
-            '누적 금액 월 지급 포인트',
-            '초과 시 익월 관리비에 부과',
-          ]"
-        />
-      </div>
-    </usage-info>
-    <info-card
-      :title="'정기결제 안내'"
-      :description="'결제 완료 후 별도 해지시까지 매월 자동으로 연장됩니다. 해지시키려면 결제내역에서 취소해주세요. 월단위 결제의 경우 이용 해지시 결제 완료된 월 말까지 이용 가능합니다.'"
-    />
-
-    <div class="usage-info-wrapper">
+    <TopTitleBar :title="'헬스장 월 이용료'" />
+    <div class="container">
+      <MonthSelecter :months="['10월', '11월']" />
       <usage-info>
+        <div class="title" slot="title">헬스장 월 이용료</div>
         <div slot="info">
-          <usage-info-text :leftText="'이용 상태'" :rightText="'이용중'" />
+          <usage-info-text :leftText="'금액'" :rightText="'월 10,000원'" />
           <usage-info-text
-            :leftText="'최초 신청일'"
-            :rightText="'2022.10.07 09:00'"
+            :leftText="'유효기간'"
+            :rightText="'10월9일 ~ 10월31일'"
           />
-          <usage-info-text :leftText="'이용 개월수'" :rightText="'1'" />
+          <usage-info-text :leftText="'결제방식'" :rightText="'관리비 청구'" />
           <usage-info-text
-            :leftText="'다음 결제일'"
-            :rightText="'2022.11.01'"
+            :leftText="'정기결제'"
+            :rightText="'매월 1일 결제'"
+            :warningMultiLineText="[
+              '월 단위 이용 가능한 프로그램입니다',
+              '유효기간 종료일을 확인해주세요',
+            ]"
+          />
+          <usage-info-text
+            :leftText="'결제안내'"
+            :multiLineText="[
+              'A-PAY 월 포인트 차감',
+              '누적 금액 월 지급 포인트',
+              '초과 시 익월 관리비에 부과',
+            ]"
           />
         </div>
       </usage-info>
-    </div>
+      <info-card
+        :title="'정기결제 안내'"
+        :description="'결제 완료 후 별도 해지시까지 매월 자동으로 연장됩니다. 해지시키려면 결제내역에서 취소해주세요. 월단위 결제의 경우 이용 해지시 결제 완료된 월 말까지 이용 가능합니다.'"
+      />
 
-    <div class="checkbox-wrapper">
-      <terms-check-box />
+      <div class="usage-info-wrapper">
+        <usage-info>
+          <div slot="info">
+            <usage-info-text :leftText="'이용 상태'" :rightText="'이용중'" />
+            <usage-info-text
+              :leftText="'최초 신청일'"
+              :rightText="'2022.10.07 09:00'"
+            />
+            <usage-info-text :leftText="'이용 개월수'" :rightText="'1'" />
+            <usage-info-text
+              :leftText="'다음 결제일'"
+              :rightText="'2022.11.01'"
+            />
+          </div>
+        </usage-info>
+      </div>
+
+      <div class="checkbox-wrapper">
+        <terms-check-box />
+      </div>
     </div>
   </div>
 </template>
@@ -62,6 +65,7 @@ import UsageInfoText from "@/components/UsageInfo/UsageInfoText.vue";
 import MonthSelecter from "@/components/DateSelecter/MonthSelecter.vue";
 import InfoCard from "@/components/InfoCard.vue";
 import TermsCheckBox from "@/components/TermsCheckBox.vue";
+import TopTitleBar from "@/components/TopTitleBar.vue";
 
 export default {
   name: "DetailRegister",
@@ -71,10 +75,14 @@ export default {
     MonthSelecter,
     InfoCard,
     TermsCheckBox,
+    TopTitleBar,
   },
 };
 </script>
 <style scoped lang="scss">
+.container {
+  padding: 0 4vw;
+}
 .title {
   font-size: 3.7333vw;
   margin-bottom: 5.3333vw;
